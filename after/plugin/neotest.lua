1ae5c637e0c3c0cf44217b246d8127e85f040ab6
stefano.boriero@gmail.com <stefano.boriero@gmail.com>
2023-06-25 23:43:13 +0200

    Add python debug configuration

--- after/plugin/neotest.lua
+++ after/plugin/neotest.lua
@@ -1,10 +1,12 @@
 require("neotest").setup({
   adapters = {
-    require("neotest-java")
+    require("neotest-java"),
+    require("neotest-python")
   }
 })
 
 require("dapui").setup()
+require('dap-python').setup()
 
 vim.keymap.set("n", "<leader>du", "<cmd>lua require('dapui').toggle()<cr>", {desc = "DAP UI"})
 vim.keymap.set("n", "<leader>db", "<cmd>lua require('dap').toggle_breakpoint()<cr>", {desc = "Toggle Breakpoint"})
@@ -13,3 +15,8 @@ vim.keymap.set("n", "<leader>dc", "<cmd>lua require('dap').continue()<cr>", {des
 vim.keymap.set("n", "<leader>dsi", "<cmd>lua require('dap').step_into()<cr>", {desc = "Step Into"})
 vim.keymap.set("n", "<leader>dso", "<cmd>lua require('dap').step_over()<cr>", {desc = "Step Over"})
 vim.keymap.set("n", "<leader>dsO", "<cmd>lua require('dap').step_out()<cr>", {desc = "Step Out"})
+
+vim.keymap.set("n", "<leader>tm", "<cmd>lua require('neotest').run.run()<cr>", {desc = "Test method"})
+vim.keymap.set("n", "<leader>dm", "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", {desc = "Debug method"})
+vim.keymap.set("n", "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", {desc = "Test file"})
+vim.keymap.set("n", "<leader>df", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'), {strategy = 'dap'})<cr>", {desc = "Debug file"})
--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -8,10 +8,16 @@ return {
         }
     },
     {
+        "nvim-neotest/neotest-python"
+    },
+    {
         'andy-bell101/neotest-java'
     },
     {
         'mfussenegger/nvim-dap'
+    },
+    {
+        'mfussenegger/nvim-dap-python'
     },
     {
         'rcarriga/nvim-dap-ui',
