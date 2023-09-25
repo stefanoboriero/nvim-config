f95e64d1771ba41d19534a85f4eea5950d3d30c1
Stefano Boriero <stefano.boriero@ocado.com>
2023-09-25 13:18:18 +0200

    Add git support for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,17 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc="Find Files"})
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, {desc="Search Grep"})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc="Find Buffers"})
-vim.keymap.set('n', '<leader>sw', builtin.grep_string, {desc="Search Word"})
+local telescope = require('telescope')
+telescope.setup {
+    pickers = {
+        find_files = {
+            hidden = true,
+            file_ignore_patterns = { ".git/" },
+        }
+    }
+}
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = "Search Grep" })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find Buffers" })
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = "Search Word" })
+vim.keymap.set('n', '<leader>sG', builtin.git_commits, { desc = "Search Git commits" })
+vim.keymap.set('n', '<leader>sbG', builtin.git_bcommits, { desc = "Search Git commits for current buffer" })
