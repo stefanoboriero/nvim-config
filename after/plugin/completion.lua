14f77ecfe396a7683b6615fc1aeb08ccc27f8339
Stefano Boriero <stefano.boriero@ocado.com>
2023-09-25 13:02:30 +0200

    Add keymaps for documentation scroll and remove border for completion
    Borders in completion were preventing autocompletion of first suggestion
    without starting to scroll the list

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -26,7 +26,10 @@ cmp.setup {
         },
     },
     window = {
-        completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
-    }
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    }),
 }
