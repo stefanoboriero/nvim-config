4cd5288077de998f55f9e3fefbe5578bcec0b4cb
Stefano Boriero <stefano.boriero@ocado.com>
2023-09-26 00:12:19 +0200

    Restore ctrl-y mapping for completion

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -31,5 +31,8 @@ cmp.setup {
     mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true })
     }),
 }
