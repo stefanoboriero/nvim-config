d07ae24db7e6452021db8af803a89a8cc69457fb
StefanoBoriero <stefano.boriero@gmail.com>
2023-09-26 23:57:26 +0200

    Register clangd lsp manually
    On the raspberry pi, I can't install clangd with Mason so I can't
    leverage mason-lspconfig for auto registration.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -29,5 +29,6 @@ end)
 lsp.skip_server_setup({ 'jdtls' })
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').clangd.setup{}
 
 lsp.setup()
