9ab8e1401de94040aee34bf885486dcfa64cd7f2
StefanoBoriero <stefano.boriero@gmail.com>
2025-08-10 15:54:07 +0200

    Removal of plugins in favour of native LSP setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,24 +1,10 @@
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    dependencies = {
-        -- LSP Support
-        { 'neovim/nvim-lspconfig' }, -- Required
-        {                            -- Optional
-            'williamboman/mason.nvim',
-            build = ":MasonUpdate",
-            opts = {
-                registries = {
-                    'github:nvim-java/mason-registry',
-                    'github:mason-org/mason-registry',
-                },
-            },
+    'williamboman/mason.nvim',
+    build = ":MasonUpdate",
+    opts = {
+        registries = {
+            'github:nvim-java/mason-registry',
+            'github:mason-org/mason-registry',
         },
-        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-
-        -- Autocompletion
-        { 'hrsh7th/nvim-cmp' },     -- Required
-        { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-        { 'L3MON4D3/LuaSnip' },     -- Required
-    }
+    },
 }
